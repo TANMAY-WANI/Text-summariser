3b783fcfa425bdccb51839789bc7b3791b5ccc86
TANMAY-WANI <tanmaywani145@gmail.com>
2024-06-03 18:40:12 +0530

    Added the requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='text_summarizer',
+    version='0.1.0',
+    author='Tanmay Wani',
+    author_email='tanmaywani145@gmail.com',
+    description='A text summarizer NLP project',
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    url="https://github.com/TANMAY-WANI/Text-summarizer"
+)
