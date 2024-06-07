919747a7373133fb68631585732eee158aaf3702
TANMAY-WANI <tanmaywani145@gmail.com>
2024-06-07 18:02:40 +0530

    Added Data Preprocessing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from textSummarizer.pipeline.data_ingestion_pipeline import data_ingestion_pipeline
+from textSummarizer.pipeline.data_preprocessing_pipeline import data_preprocessing_pipeline
 from textSummarizer.logging import logger
 
 cur_stage = "Data Ingestion"
@@ -9,6 +10,17 @@ try:
     data.main()
     logger.info(f"Stage {cur_stage} finished")
 
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+cur_stage = "Data Preprocessing"
+
+try:
+    logger.info(f"Stage: {cur_stage} started")
+    dp = data_preprocessing_pipeline()
+    dp.main()
+    logger.info(f"Stage {cur_stage} successfully completed")
 except Exception as e:
     logger.exception(e)
     raise e
